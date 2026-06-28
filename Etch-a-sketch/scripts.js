d8a9e0e4ed611cc7602b662b8bd9ba12a52c2b76
benzo1233 <benztong007@gmail.com>
2026-06-28 18:01:38 -0400

    first working iteration

---
+++ Etch-a-sketch/scripts.js
@@ -0,0 +1,52 @@
+
+
+// TASK:
+// Make a 16x16 sketch board
+// Make row of 16 elements:
+/* ==== TODO ===== 
+Creating div element to contain 16 elements, using flexbox
+Append 16 divs to it, perhaps intially making them 10x10pixels with black border
+*/
+
+//Make the row 16 times 
+/*  ===== TODO =====
+encapsulate code above and run it 16 times.
+*/
+
+// Further Note: Make it a func?
+
+const generateBoard = document.querySelector("#generateBoard");
+const board = document.querySelector("#board");
+
+function createRow () {
+    const container = document.createElement("div");
+    container.id = container;
+
+    for (let i = 0; i < 8; i++) {
+        const div = document.createElement("button");
+        div.style.width= "25px";
+        div.style.height= "25px";
+        container.appendChild(div);
+
+        div.addEventListener("mouseenter", () => div.style.background = "orange");
+    }
+    board.appendChild(container);
+}
+
+function createCol () {
+    for (let i = 0; i < 8; i++) {
+        createRow();
+    }
+    board.appendChild(container);
+}
+
+function createBoard () {
+    createCol(8);
+}
+
+
+
+generateBoard.addEventListener("click", () => createBoard());
+
+
+
