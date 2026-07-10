e251621939c69291e781eb87a0a5789cbbe17940
benzo1233 <benztong007@gmail.com>
2026-07-09 20:54:16 -0400

    added sqrt functionality & cleaner implementation of operate

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -17,7 +17,13 @@ numButtons.forEach((num) => {
 
 operatorButtons.forEach((operator) => {
     operator.addEventListener("click", (e) => {
-        if (firstOperand && currOperator && currInput) {
+        if (e.target.textContent === "SQRT" && (firstOperand || currInput)) {
+            const val = currInput !== "" ? Number(currInput) : firstOperand;
+            firstOperand = operate(null, val, "SQRT");
+            displayElement.textContent = firstOperand;
+            currInput = "";
+            return;
+        } else if (firstOperand && currOperator && currInput) {
             firstOperand = operate(firstOperand, Number(currInput), currOperator);
             displayElement.textContent = firstOperand;
         } else if (currInput !== "") {
@@ -48,41 +54,39 @@ clearButton.addEventListener("click", () => {
 });
 
 function operate(num1, num2, operator) {
-    const op =
-        operator === "+"
-            ? add(num1, num2)
-            : operator === "-"
-                ? sub(num1, num2)
-                : operator === "*"
-                    ? multi(num1, num2)
-                    : operator === "/"
-                        ? div(num1, num2)
-                        : "ERR";
-
-    return Number(op);
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return sub(num1, num2);
+        case "*":
+            return multi(num1, num2)
+        case "/":
+            return div(num1, num2);
+        case "%":
+            return modulo(num1, num2);
+        case "SQRT":
+            return sqrt(num2);
+        default:
+            return "ERR";
+    }
 };
 
-function add(num1, num2) {
-    return num1 + num2;
-};
+function add(num1, num2) { return num1 + num2; };
 
-function sub(num1, num2) {
-    return num1 - num2;
-};
+function sub(num1, num2) { return num1 - num2; };
 
-function multi(num1, num2) {
-    return num1 * num2;
-};
+function multi(num1, num2) { return num1 * num2; };
 
-function div(num1, num2) {
-    return num1 / num2;
-};
+function div(num1, num2) { return num1 / num2; };
+
+function modulo(num1, num2) { return num1 % num2; };
+
+function sqrt(num2) { return Math.sqrt(num2); };
 
 function reset() {
     displayElement.textContent = "";
     currInput = "";
     firstOperand = null;
     currOperator = "";
-
-    console.log(`RESET: ${firstOperand}`);
 };
