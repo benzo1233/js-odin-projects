ce6560919ced2cd420cd5b2cf80a0a886b9bfd0f
benzo1233 <benztong007@gmail.com>
2026-07-09 21:40:13 -0400

    implement event delegation for buttons

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,43 +1,83 @@
-const numButtons = document.querySelectorAll(".num");
-const operatorButtons = document.querySelectorAll(".operator");
-const clearButton = document.querySelector(".clear");
-const enterButton = document.querySelector(".equal");
+// const numButtons = document.querySelectorAll(".num");
+// const operatorButtons = document.querySelectorAll(".operator");
+// const clearButton = document.querySelector(".clear");
+// const enterButton = document.querySelector(".equal");
+
+//new
+const buttons = document.querySelector(".calculator");
+
 let displayElement = document.querySelector(".display");
 
 let currInput = "";
 let currOperator = "";
 let firstOperand = null;
 
-numButtons.forEach((num) => {
-    num.addEventListener("click", (e) => {
-        currInput += e.target.textContent;
-        displayElement.textContent = currInput;
-    });
+// numButtons.forEach((num) => {
+//     num.addEventListener("click", (e) => {
+//         numHandler(e);
+//     });
+// });
+
+// operatorButtons.forEach((button) => {
+//     button.addEventListener("click", (e) => {
+//         operatorHandler(e);
+//     });
+// });
+
+// enterButton.addEventListener("click", () => {
+//     enterHandler();
+// });
+
+// clearButton.addEventListener("click", () => {
+//     reset();
+// });
+
+
+buttons.addEventListener("click", (e) => {
+    const buttonContent = e.target.classList;
+    if (buttonContent.contains("num")) {
+        numHandler(e);
+    }
+
+    if (buttonContent.contains("operator")) {
+        operatorHandler(e);
+    }
+
+    if (buttonContent.contains("equal")) {
+        enterHandler();
+    }
+
+    if (buttonContent.contains("clear")) {
+        reset();
+    }
 });
 
-operatorButtons.forEach((operator) => {
-    operator.addEventListener("click", (e) => {
-        if (e.target.textContent === "SQRT" && (firstOperand || currInput)) {
-            const val = currInput !== "" ? Number(currInput) : firstOperand;
-            firstOperand = operate(null, val, "SQRT");
-            displayElement.textContent = firstOperand;
-            currInput = "";
-            return;
-        } else if (firstOperand && currOperator && currInput) {
-            firstOperand = operate(firstOperand, Number(currInput), currOperator);
-            displayElement.textContent = firstOperand;
-        } else if (currInput !== "") {
-            firstOperand = Number(currInput);
-        }
-
-        currOperator = e.target.textContent;
+function operatorHandler (e) {
+    if (e.target.textContent === "SQRT" && (firstOperand || currInput)) {
+        const val = currInput !== "" ? Number(currInput) : firstOperand;
+        firstOperand = operate(null, val, "SQRT");
+        displayElement.textContent = firstOperand;
         currInput = "";
-    });
-});
+        return;
+    } else if (firstOperand && currOperator && currInput) {
+        firstOperand = operate(firstOperand, Number(currInput), currOperator);
+        displayElement.textContent = firstOperand;
+    } else if (currInput !== "") {
+        firstOperand = Number(currInput);
+    }
 
-enterButton.addEventListener("click", () => {
+    currOperator = e.target.textContent;
+    currInput = "";
+}
+
+function numHandler (e) {
+    currInput += e.target.textContent;
+    displayElement.textContent = currInput;
+}
+
+function enterHandler (e) {
     if (!firstOperand && currOperator === "" && currInput === "") {
-        console.log("INVALID FIELDS");
+        displayElement.textContent = ("put something vro");
         return;
     }
 
@@ -47,11 +87,7 @@ enterButton.addEventListener("click", () => {
 
     currInput = "";
     currOperator = "";
-});
-
-clearButton.addEventListener("click", () => {
-    reset();
-});
+}
 
 function operate(num1, num2, operator) {
     switch (operator) {
