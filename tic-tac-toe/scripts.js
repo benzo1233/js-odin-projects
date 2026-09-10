7a218f140fe8cfdf71cd4f62109f4c76edca4339
benzo1233 <benztong007@gmail.com>
2026-09-10 16:43:37 -0400

    first working tictactoe

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -10,7 +10,22 @@ const gameBoard = (() => {
     let rows = 3;
     let cols = 3;
     let board = [];
-    let gameStatus = false;
+    let gameOverStatus = false;
+    const winningLines = [
+        //Row
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+
+        //Col
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+
+        //Diag
+        [[0, 0], [1, 1], [2, 2]],
+        [[2, 0], [1, 1], [0, 2]],
+    ];
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
@@ -20,7 +35,7 @@ const gameBoard = (() => {
     };
 
     function placeTic(row, col, marker) {
-        if (gameStatus === true) {
+        if (gameOverStatus === true) {
             console.log("GameOVER!!")
             return;
         }
@@ -30,18 +45,35 @@ const gameBoard = (() => {
         }
         board[row][col] = marker;
         console.log("Tic Placed!");
-        console.log(board);
+        console.log(row, col);
         return true;
     };
 
     // for testing
     function setWin(status) {
-        gameStatus = status;
+        gameOverStatus = status;
     }
+
+    function checkWin(name) {
+        for (const line of winningLines) {
+            // const [p1, p2, p3] = line;
+            // let a1 = board[p1[0], p1[1]];
+            // let a2 = board[p2[0], p2[1]];
+            // let a3 = board[p3[0], p3[1]];
+            // OR
+            const [p1, p2, p3] = line.map(([r, c]) => board[r][c]);
+            if (p1 !== null && p1 === p2 && p1 === p3) {
+                console.log(`We have a Winner: ${name}`)
+                setWin(true);
+            }
+        }
+    }
+
 
     return {
         placeTic,
         setWin,
+        checkWin,
     }
 })();
 
@@ -52,28 +84,65 @@ function createPlayer(name, marker) {
     };
 }
 
-
 const controller = (() => {
     const player1 = createPlayer("Ben", "X");
     const player2 = createPlayer("Kenny", "O");
 
     let currentPlayer = player1;
-    function playGame (row, col) {
+    function playGame(row, col) {
         const placed = gameBoard.placeTic(row, col, currentPlayer.marker);
         if (placed === true) {
+            view.updateView(row, col, currentPlayer.marker);
+            gameBoard.checkWin(currentPlayer.name)
             currentPlayer = currentPlayer === player1 ? player2 : player1;
         }
     }
 
-    function getCurrentPlayer () {
+    function getCurrentPlayer() {
         return currentPlayer;
     }
-    
+
     return {
         playGame,
         getCurrentPlayer
     }
 })();
 
+
+const body = document.getElementById("body");
+const view = {
+    createView() {
+        for (let i = 0; i < 3; i++) {
+            const row = document.createElement("div");
+            for (let j = 0; j < 3; j++) {
+                const button = document.createElement("button");
+                button.textContent = "-";
+                button.dataset.row = i;
+                button.dataset.col = j;
+                row.appendChild(button);
+            }
+            body.appendChild(row);
+        }
+    },
+
+    updateView(row, col, marker) {
+        const button = document.querySelector(
+            `[data-row="${row}"][data-col="${col}"]`
+        );
+        button.textContent = marker;
+    }
+};
+
+body.addEventListener("click", (e) => {
+    const target = e.target.dataset;
+    if (!e.target.matches("button")) { return }
+
+    const row = Number(target.row);
+    const col = Number(target.col);
+    console.log(controller.getCurrentPlayer());
+    controller.playGame(row, col);
+});
+
+view.createView();
 
 
