f286394f35928751c555e1846caa94424b52811f
benzo1233 <benztong007@gmail.com>
2026-09-22 21:41:20 -0400

    rough draft restaurant page

--- restaurant/README.md
+++ restaurant/README.md
@@ -3,11 +3,11 @@
 Restaurant page for practicing DOM manipulation by dynamically rendering a restaurant homepage.
 It is also an oppurtunity to practice using the webpack bundler and configurations.
 
-## Features / Want to's
+## Want To's
 
 - Three Dynamically rendered homepages
 - Mobile First Dev
-- TBD
+- DOM elements should be created using JavaScript but styling can be done in a separate CSS file
 
 ## Installation
 
--- restaurant/webpack.config.js
+++ restaurant/webpack.config.js
@@ -15,4 +15,28 @@ export default {
             template: "./src/index.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            // {
+            //     test: /\.html$/i,
+            //     use: ["html-loader"],
+            // },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff2?|woff?|ttf|otf|eot)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name][ext]"
+                }
+            }
+
+        ],
+    },
 };
